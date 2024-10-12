0a61dc4ad838efe91e2f48cd75889b919acc63e9
Datureli <soleris1@gmail.com>
2024-10-12 19:00:29 +0200

    add option for run test with node and dotenv

---
+++ runTests.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const newman = require('newman'); // require newman in your project
+
+newman.run({
+    collection: require('./postman/Mock-bank_api.json'), // Upewnij się, że ścieżka do pliku JSON jest poprawna
+    reporters: 'cli',
+    environment: {
+        "values": [
+            {
+                "key": "MOCK_API_URL",
+                "value": process.env.MOCK_API_URL,
+                "enabled": true
+            }
+        ]
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('Collection run complete!');
+});
