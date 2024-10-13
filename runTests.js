75d97439f480faf0be1466ac41171534e41bea15
Datureli <soleris1@gmail.com>
2024-10-13 20:42:04 +0200

    add dynamic mock api path

--- runTests.js
+++ runTests.js
@@ -1,19 +1,18 @@
-require('dotenv').config();
+require('dotenv').config(); // Ładuje zmienne z pliku .env
 const newman = require('newman');
 
+// Uruchamianie kolekcji Postmana, dynamicznie wstawiając URL API
 newman.run({
-    collection: require('./postman/Mock-bank_api.json'), // Upewnij się, że ścieżka do pliku JSON jest poprawna
-    reporters: 'cli',
+    collection: require('./postman/Mock-bank_api.json'), // Ścieżka do twojej kolekcji
     environment: {
-        "values": [
-            {
-                "key": "MOCK_API_URL",
-                "value": process.env.MOCK_API_URL,
-                "enabled": true
-            }
+        values: [
+            { key: 'MOCK_API_URL', value: process.env.MOCK_API_URL },
+            { key: 'TOKEN', value: process.env.TOKEN }
         ]
     }
-}, function (err) {
-    if (err) { throw err; }
-    console.log('Collection run complete!');
+}, function (err, summary) {
+    if (err) {
+        throw err;
+    }
+    console.log('Testy zakończone.');
 });
