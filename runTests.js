4f8892d82162effa3ac895fdd764d07a9fbb0be8
Datureli <soleris1@gmail.com>
2024-10-20 22:04:44 +0200

    improved test import in the runTests.js file

--- runTests.js
+++ runTests.js
@@ -1,54 +1,37 @@
-require('dotenv').config(); // Wczytuje zmienne z .env
-const newman = require('newman'); // Ładuje Newmana
+require('dotenv').config();
+const newman = require('newman');
 const fs = require('fs');
+const path = require('path');
 
-// Wczytaj kod testowy z pliku balanceTests.js
-const balanceTestsScript = fs.readFileSync('./postman-test-scripts/balanceTests.js', 'utf-8');
-const transactionTestsScript = fs.readFileSync('./postman-test-scripts/transactionTests.js', 'utf-8');
-
-// Wczytaj kolekcję Postmana
 const collection = require('./postman/Mock-bank_api.json');
 
-// Dodaj sekcję 'event' i 'script' do requestu "Sprawdzenie salda", jeśli nie istnieje
-if (!collection.item[0].event) {
-    collection.item[0].event = [{ listen: 'test', script: { exec: [] } }];
-}
+const scriptsDir = './postman-test-scripts';
+const testFiles = fs.readdirSync(scriptsDir).filter(file => file.endsWith('.js'));
 
-// Przypisz kod testowy z pliku balanceTests.js
-collection.item[0].event[0].script.exec = balanceTestsScript.split('\n'); // Dodaj kod do pierwszego requestu
+testFiles.forEach((file, index) => {
+    const scriptContent = fs.readFileSync(path.join(scriptsDir, file), 'utf-8');
 
-// Dodaj sekcję 'event' i 'script' do requestu "Historia transakcji", jeśli nie istnieje
-if (!collection.item[1].event) {
-    collection.item[1].event = [{ listen: 'test', script: { exec: [] } }];
-}
-
-// Przypisz kod testowy z pliku transactionTests.js
-collection.item[1].event[0].script.exec = transactionTestsScript.split('\n'); // Dodaj kod do drugiego requestu
-
-// Uruchomienie Newmana z dynamicznymi zmiennymi
-newman.run({
-    collection: collection, // Użyj zaktualizowanej kolekcji
-    reporters: 'cli',
-    environment: {
-        "values": [
-            {
-                "key": "MOCK_API_URL",
-                "value": process.env.MOCK_API_URL, // Pobieranie z .env
-                "enabled": true
-            },
-            {
-                "key": "MOCK_API_PATH",
-                "value": process.env.MOCK_API_PATH, // Pobieranie z .env
-                "enabled": true
-            },
-            {
-                "key": "TOKEN",
-                "value": process.env.TOKEN, // Pobieranie z .env
-                "enabled": true
-            }
-        ]
+    if (!collection.item[index].event) {
+        collection.item[index].event = [{ listen: 'test', script: { exec: [] } }];
     }
-}, function (err, summary) {
-    if (err) { throw err; }
-    console.log('Testy zostały zakończone.');
+
+    collection.item[index].event[0].script.exec = scriptContent.split('\n');
 });
+
+collection.item.forEach((item, index) => {
+    const apiUrl = `${process.env.MOCK_API_URL}/v3/${item.request.url.raw.split('/').pop()}`;
+    item.request.url.raw = apiUrl;
+});
+
+newman.run(
+    {
+        collection: collection,
+        reporters: 'cli',
+    },
+    function (err, summary) {
+        if (err) {
+            throw err;
+        }
+        console.log('Testy zostały zakończone.');
+    }
+);
