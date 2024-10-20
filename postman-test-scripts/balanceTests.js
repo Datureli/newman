5562bec2f9694597260de2bb5b33cc6c6ccc4dee
Datureli <soleris1@gmail.com>
2024-10-20 22:33:55 +0200

    improving postman tests according to the es6+ standard

--- postman-test-scripts/balanceTests.js
+++ postman-test-scripts/balanceTests.js
@@ -1,25 +1,25 @@
 const jsonData = pm.response.json();
 
-pm.test('Sprawdź, czy saldo (balance) istnieje', function () {
-    pm.expect(jsonData.accounts[0].balance).to.exist;
+pm.test('Sprawdź, czy saldo (balance) istnieje', () => {
+    pm.expect(jsonData?.accounts?.[0]?.balance).to.exist;
 });
 
-pm.test('Saldo (balance) jest liczbą', function () {
-    pm.expect(jsonData.accounts[0].balance).to.be.a('number');
+pm.test('Saldo (balance) jest liczbą', () => {
+    pm.expect(jsonData?.accounts?.[0]?.balance).to.be.a('number');
 });
 
-pm.test('Saldo (balance) jest większe od zera', function () {
-    pm.expect(jsonData.accounts[0].balance).to.be.greaterThan(0);
+pm.test('Saldo (balance) jest większe od zera', () => {
+    pm.expect(jsonData?.accounts?.[0]?.balance).to.be.greaterThan(0);
 });
 
-pm.test('Waluta jest poprawna', function () {
-    pm.expect(jsonData.accounts[0].currency).to.equal('USD');
+pm.test('Waluta jest poprawna', () => {
+    pm.expect(jsonData?.accounts?.[0]?.currency).to.equal('USD');
 });
 
-pm.test('Nagłówek Content-Type jest poprawny', function () {
+pm.test('Nagłówek Content-Type jest poprawny', () => {
     pm.expect(pm.response.headers.get('Content-Type')).to.include('application/json');
 });
 
-pm.test('Czas odpowiedzi jest poniżej 500ms', function () {
+pm.test('Czas odpowiedzi jest poniżej 500ms', () => {
     pm.expect(pm.response.responseTime).to.be.below(500);
 });
--- postman-test-scripts/transactionTests.js
+++ postman-test-scripts/transactionTests.js
@@ -1,32 +1,32 @@
-// transactionTests.js
 const jsonData = pm.response.json();
 
-pm.test('Sprawdź czy lista transakcji istnieje', function () {
-    pm.expect(jsonData.transactions).to.exist;
+pm.test('Sprawdź czy lista transakcji istnieje', () => {
+    pm.expect(jsonData?.transactions).to.exist;
 });
 
-pm.test('Transakcje to tablica', function () {
-    pm.expect(jsonData.transactions).to.be.an('array');
+pm.test('Transakcje to tablica', () => {
+    pm.expect(jsonData?.transactions).to.be.an('array');
 });
 
-pm.test('Każda transakcja ma pole amount', function () {
-    jsonData.transactions.forEach(function (transaction) {
-        pm.expect(transaction.amount).to.exist;
+pm.test('Każda transakcja ma pole amount', () => {
+    jsonData?.transactions?.forEach(transaction => {
+        pm.expect(transaction?.amount).to.exist;
     });
 });
 
-jsonData.transactions.forEach(function (transaction) {
-    pm.test(`Transakcja ${transaction.id} zawiera wszystkie wymagane pola`, function () {
+jsonData?.transactions?.forEach(transaction => {
+    pm.test(`Transakcja ${transaction?.id} zawiera wszystkie wymagane pola`, () => {
         pm.expect(transaction).to.have.all.keys('id', 'amount', 'type', 'date');
     });
-    pm.test(`Typ transakcji ${transaction.id} jest poprawny`, function () {
-        pm.expect(transaction.type).to.satisfy(type => ['deposit', 'withdrawal'].includes(type));
+
+    pm.test(`Typ transakcji ${transaction?.id} jest poprawny`, () => {
+        pm.expect(transaction?.type).to.satisfy(type => ['deposit', 'withdrawal'].includes(type));
     });
 });
 
-jsonData.transactions.forEach(function (transaction) {
-    pm.test(`Data transakcji ${transaction.id} jest poprawna`, function () {
-        const transactionDate = new Date(transaction.date);
+jsonData?.transactions?.forEach(transaction => {
+    pm.test(`Data transakcji ${transaction?.id} jest poprawna`, () => {
+        const transactionDate = new Date(transaction?.date);
         const isValidDate = !isNaN(transactionDate.getTime());
         pm.expect(isValidDate).to.be.true;
     });
