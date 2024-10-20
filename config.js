34b3c3724b31cd3d8e25656efca92f83a9c64ce2
Datureli <soleris1@gmail.com>
2024-10-20 22:28:18 +0200

    remove unused files

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-require('dotenv').config();
-
-module.exports = {
-  MOCK_API_URL: process.env.MOCK_API_URL,
-  MOCK_API_PATH: process.env.MOCK_API_PATH,
-  TRANSACTIONS_PATH: process.env.TRANSACTIONS_PATH,
-  TOKEN: process.env.TOKEN
-};
