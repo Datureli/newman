260a5e4b6aa50d4f92d5c7616cc72f64b8f38f5f
Datureli <soleris1@gmail.com>
2024-10-21 20:06:20 +0200

    use dynamic api url in runTests.js and mock bank api

--- runTests.js
+++ runTests.js
@@ -3,22 +3,45 @@ const newman = require('newman');
 const fs = require('fs');
 const path = require('path');
 
+// Wczytanie kolekcji Postmana
 const collection = require('./postman/Mock-bank_api.json');
 
+// Wczytanie skryptów testowych
 const scriptsDir = './postman-test-scripts';
 const testFiles = fs.readdirSync(scriptsDir).filter(file => file.endsWith('.js'));
 
+// Przypisanie skryptów do odpowiednich elementów w kolekcji
 testFiles.forEach((file, index) => {
   const scriptContent = fs.readFileSync(path.join(scriptsDir, file), 'utf-8');
   collection.item[index].event = collection.item[index].event || [{ listen: 'test', script: { exec: [] } }];
   collection.item[index].event[0].script.exec = scriptContent.split('\n');
 });
 
+// Dynamiczne ustawienie URL-i z pliku .env
 collection.item.forEach(item => {
-  const apiPath = item.request.url.raw.split('/').pop();
-  item.request.url.raw = `${process.env.MOCK_API_URL}/v3/${apiPath}`;
+  if (item.name === "Sprawdzenie salda") {
+    item.request.url = {
+      raw: `${process.env.MOCK_API_URL}/${process.env.MOCK_API_BALANCE_PATH}`,
+      protocol: 'https',
+      host: process.env.MOCK_API_URL.replace('https://', '').split('.'),
+      path: process.env.MOCK_API_BALANCE_PATH.split('/')
+    };
+  } else if (item.name === "Historia transakcji") {
+    item.request.url = {
+      raw: `${process.env.MOCK_API_URL}/${process.env.MOCK_API_TRANSACTIONS_PATH}`,
+      protocol: 'https',
+      host: process.env.MOCK_API_URL.replace('https://', '').split('.'),
+      path: process.env.MOCK_API_TRANSACTIONS_PATH.split('/')
+    };
+  }
 });
 
+// Debugging: Wyświetlenie przetworzonych URL-i
+collection.item.forEach(item => {
+  console.log(`URL for ${item.name}: ${item.request.url.raw}`);
+});
+
+// Uruchomienie testów przez Newmana
 newman.run({ collection, reporters: 'cli' }, (err, summary) => {
   if (err) throw err;
   console.log('Testy zostały zakończone.');
