dff5e87aa7fb47a8ac6d50d07c9a031dac57bc06
Datureli <soleris1@gmail.com>
2024-10-21 23:45:42 +0200

    fix tests

--- postman-test-scripts/loginTests/userLoginTests.js
+++ postman-test-scripts/loginTests/userLoginTests.js
@@ -1,14 +1,13 @@
-// loginTestScript.js
 pm.test("Status code is 200", function () {
     pm.response.to.have.status(200);
 });
 
-pm.test("Response contains token", function () {
+pm.test("Znaleziono użytkownika", function () {
     var jsonData = pm.response.json();
-    pm.expect(jsonData).to.have.property("token");
+    pm.expect(jsonData.users).to.be.an('array').that.is.not.empty; // Sprawdza, czy tablica użytkowników nie jest pusta
+    pm.expect(jsonData.users[0].username).to.eql("john_doe"); // Sprawdza, czy pierwszy użytkownik to john_doe
 });
 
-pm.test("Response token is not empty", function () {
-    var jsonData = pm.response.json();
-    pm.expect(jsonData.token).to.not.be.empty;
+pm.test("Nie ma tokenu", function () {
+    pm.expect(pm.response.json()).to.not.have.property('token'); // Sprawdza, czy nie ma tokenu
 });
