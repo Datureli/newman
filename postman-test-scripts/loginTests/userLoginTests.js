cc3ffed9f2ceda84ea27582ef3dc3a0d77f5e13b
Datureli <soleris1@gmail.com>
2024-10-23 20:09:41 +0200

    add login tests for checking username and password

--- postman-test-scripts/loginTests/userLoginTests.js
+++ postman-test-scripts/loginTests/userLoginTests.js
@@ -1,13 +1,21 @@
-pm.test("Status code is 200", function () {
+pm.test("Status code is 200", () => {
     pm.response.to.have.status(200);
 });
 
-pm.test("Znaleziono użytkownika", function () {
-    var jsonData = pm.response.json();
-    pm.expect(jsonData.users).to.be.an('array').that.is.not.empty; // Sprawdza, czy tablica użytkowników nie jest pusta
-    pm.expect(jsonData.users[0].username).to.eql("john_doe"); // Sprawdza, czy pierwszy użytkownik to john_doe
+pm.test("Znaleziono użytkownika", () => {
+    const jsonData = pm.response.json();
+    pm.expect(jsonData.users).to.be.an('array').that.is.not.empty;
+    pm.expect(jsonData.users[0].username).to.eql("john_doe");
 });
 
-pm.test("Nie ma tokenu", function () {
-    pm.expect(pm.response.json()).to.not.have.property('token'); // Sprawdza, czy nie ma tokenu
+pm.test("Nie ma tokenu", () => {
+    pm.expect(pm.response.json()).to.not.have.property('token');
+});
+
+pm.test("Próba logowania z podanym username i password", () => {
+    const jsonData = pm.response.json();
+    const expectedUsername = "john_doe";
+    const expectedPassword = "John!12345";
+    const userExists = jsonData.users.some(user => user.username === expectedUsername && user.password === expectedPassword);
+    pm.expect(userExists).to.be.true;
 });
