30919365dd2b9287acb9f877d5b4f805fb708448
Datureli <soleris1@gmail.com>
2024-10-30 16:01:59 +0100

    add expiration date to token

--- postman-test-scripts/loginTests/preRequestScript.js
+++ postman-test-scripts/loginTests/preRequestScript.js
@@ -1,14 +1,27 @@
-const header = { "alg": "HS256", "typ": "JWT" };
-const payload = { "iat": Math.floor(Date.now() / 1000) }; // tylko czas wygenerowania
+const header = { alg: "HS256", typ: "JWT" };
+const payload = {
+  iat: Math.floor(Date.now() / 1000),
+  exp: Math.floor(Date.now() / 1000) + 3600,
+};
 const secretKey = "TwojSekretnyKlucz";
 
 function base64urlEncode(source) {
-    return CryptoJS.enc.Base64.stringify(source).replace(/=+$/, '').replace(/\+/g, '-').replace(/\//g, '_');
+  return CryptoJS.enc.Base64.stringify(source)
+    .replace(/=+$/, "")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
 }
 
-const encodedHeader = base64urlEncode(CryptoJS.enc.Utf8.parse(JSON.stringify(header)));
-const encodedPayload = base64urlEncode(CryptoJS.enc.Utf8.parse(JSON.stringify(payload)));
-const signature = CryptoJS.HmacSHA256(encodedHeader + "." + encodedPayload, secretKey);
+const encodedHeader = base64urlEncode(
+  CryptoJS.enc.Utf8.parse(JSON.stringify(header))
+);
+const encodedPayload = base64urlEncode(
+  CryptoJS.enc.Utf8.parse(JSON.stringify(payload))
+);
+const signature = CryptoJS.HmacSHA256(
+  encodedHeader + "." + encodedPayload,
+  secretKey
+);
 const encodedSignature = base64urlEncode(signature);
 
 const jwtToken = `${encodedHeader}.${encodedPayload}.${encodedSignature}`;
