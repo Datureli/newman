ed6895dc97d71957bae3519edccfe18bd16ff781
Datureli <soleris1@gmail.com>
2024-11-02 14:54:45 +0100

    remove unused files

--- supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://<your_project_ref>.supabase.co';
-const supabaseKey = '<your_public_anon_key>';
-const supabase = createClient(supabaseUrl, supabaseKey);
