685eb9007a5a6ae086d17294661cdc8e1b008ee7
Datureli <soleris1@gmail.com>
2024-11-07 16:56:20 +0100

    change sql files path and remove unused files

---
+++ sql-tests/tests/users_table_test.sql
@@ -0,0 +1,4 @@
+source sql-tests/schemas/01_create_user_table.sql
+source sql-tests/procedures/add_user.sql
+source sql-tests/queries/check_unique_usernames.sql
+source sql-tests/seeds/add_test_user.sql
