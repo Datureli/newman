083164fcd873af0a1a443e4e5ed67865f86bfa88
Datureli <soleris1@gmail.com>
2024-11-14 13:27:46 +0100

    fix path to role menagement file

--- sql-tests/tests/add_permission_test.sql
+++ sql-tests/tests/add_permission_test.sql
@@ -1,8 +1,8 @@
-source sql-tests/schemas/role_menagement/02_create_roles.sql
-source sql-tests/schemas/role_menagement/add_permissions_to_roles.sql
-source sql-tests/seeds/role_menagement/permission_data.sql
-source sql-tests/seeds/role_menagement/role_permission_data.sql
-source sql-tests/queries/role_menagement/get_admin_actions.sql
-source sql-tests/queries/role_menagement/role_admin_actions.sql
-source sql-tests/queries/role_menagement/role_admin_unique_actions.sql
-source sql-tests/queries/role_menagement/tester_user_delete_post.sql
+source sql-tests/role_menagement/schemas/02_create_roles.sql
+source sql-tests/role_menagement/schemas/add_permissions_to_roles.sql
+source sql-tests/role_menagement/seeds/permission_data.sql
+source sql-tests/role_menagement/seeds/role_permission_data.sql
+source sql-tests/role_menagement/queries/get_admin_actions.sql
+source sql-tests/role_menagement/queries/role_admin_actions.sql
+source sql-tests/role_menagement/queries/role_admin_unique_actions.sql
+source sql-tests/role_menagement/queries/tester_user_delete_post.sql
