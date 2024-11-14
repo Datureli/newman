5fbe381879564ce3f5a9048185691a876d8cd1ff
Datureli <soleris1@gmail.com>
2024-11-14 18:18:52 +0100

    fix file path to user table

--- sql-tests/tests/users_table_test.sql
+++ sql-tests/tests/users_table_test.sql
@@ -1,4 +1,4 @@
-source sql-tests/schemas/01_create_user_table.sql
-source sql-tests/procedures/add_user.sql
-source sql-tests/queries/check_unique_usernames.sql
-source sql-tests/seeds/add_test_user.sql
+source sql-tests/users_menagement/add_user/schemas/01_create_user_table.sql
+source sql-tests/users_menagement/add_user/procedures/add_user.sql
+source sql-tests/users_menagement/add_user/queries/check_unique_usernames.sql
+source sql-tests/users_menagement/add_user/seeds/add_test_user.sql
