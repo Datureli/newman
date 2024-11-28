d9334aefb45dd667c859a404dd49b3ac7fe0950a
Datureli <soleris1@gmail.com>
2024-11-28 13:57:22 +0100

    fix visibility of GetBloodCompatibilityView file

--- oracle-sql/view/GetBloodCompatibilityView.sql
+++ oracle-sql/view/GetBloodCompatibilityView.sql
@@ -1,9 +1,10 @@
 CREATE OR REPLACE VIEW DonorCompatibility AS
 SELECT 
-    d.Name || ' ' || d.Surname AS DonorName, -- Pełne imię i nazwisko
+    (d.Name || ' ' || d.Surname) AS DonorName, 
     d.BloodType AS DonorBloodType,
-    LISTAGG(r.Name || ' ' || r.Surname || ' (' || r.BloodType || ')', ', ') 
-        WITHIN GROUP (ORDER BY r.Name || ' ' || r.Surname) AS CompatibleRecipients
+    LISTAGG(
+        (r.Name || ' ' || r.Surname || ' (' || r.BloodType || ')'), ', ') 
+        WITHIN GROUP (ORDER BY r.Name, r.Surname) AS CompatibleRecipients
 FROM 
     Soldiers d
 JOIN 
@@ -12,8 +13,8 @@ JOIN
 JOIN 
     Soldiers r 
     ON r.BloodType = bc.RecipientBloodType
-    AND d.SoldierID != r.SoldierID  -- Wykluczenie tego samego żołnierza
+    AND d.SoldierID != r.SoldierID
 GROUP BY 
-    d.Name || ' ' || d.Surname, d.BloodType
+    d.Name, d.Surname, d.BloodType
 ORDER BY 
-    d.Name || ' ' || d.Surname;
+    d.Name, d.Surname;
